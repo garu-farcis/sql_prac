001c4e2c9fee82009b45fd5026f46d58514fc100
garu-farcis <priyankagaru2024@gmail.com>
2026-09-13 17:24:11 +0200

    update

--- L8.sql
+++ L8.sql
@@ -9,3 +9,18 @@ sum(pa.amount) over (partition by cu.customer_id  order by pa.payment_date asc
             rows between unbounded preceding and current row ) as running_total
 from customer cu left join payment pa 
 on cu.customer_id=pa.customer_id;
+
+-- 2. Rank all films by the number of times they have been rented (most rented
+--    first). Display film title, rental count, and the rank. Films with the
+--    same rental count must share the same rank, and the next rank must skip
+--    accordingly.
+
+select ff.title, 
+count(*) as rental_count,
+rank() over (order by count(re.rental_id)) as ranking
+from film ff left join inventory inv
+on ff.film_id=inv.film_id
+left join rental re
+on inv.inventory_id=re.inventory_id
+group by ff.film_id,ff.title
+order by ranking;
